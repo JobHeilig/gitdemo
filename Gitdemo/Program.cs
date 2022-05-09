81592628a88c7a8d8dede1ff88bb0ab445075a6c
JobHeilig <jobheilig@gmail.com>
2022-05-09 09:49:25 +0200

    initial commit

---
+++ Gitdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gitdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
